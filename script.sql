a4dd3c44c4aa1f6c38a2ce567ffc1c0c9eed74b6
wng1 <wng1@users.noreply.github.com>
2016-03-16 14:16:39 +0000

    Update script.sql

--- script.sql
+++ script.sql
@@ -1,1 +1,13 @@
+CREATE TABLE Customers
+(
+PersonID int,
+FirstName varchar(50),
+LastName varchar(50),
+Address1 varchar(50),
+Address2 varchar(50),
+Address3 varchar(50),
+); 
 
+//Small quick sample
+
+//larger scripts contains commits and rollback
