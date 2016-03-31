0c01fad91903f6b31d9ff5bc2a7e642a626d369f
wng1 <wng1@users.noreply.github.com>
2016-03-31 20:54:43 +0100

    Update three.sql

--- variables/three.sql
+++ variables/three.sql
@@ -1,7 +1,9 @@
 SELECT
    YOD(O.orderDate) AS OrderYear,
    SUM(OD.qty) AS Quantity
-INTO dbo.#MyOrderSalesTotalByYear
+INTO dbo.#MyOrderSalesTotalByYear   //local temporary tables prefixed with hash
 
 FROM Sales.Orders AS O   //entities 
-
+JOIN Sales.OrderDetails AS OD
+ON OD.orderID = O.orderID
+GROUP BY YOD(orderDate);
