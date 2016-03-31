93f2748b2da11756e60071f9d7a8781a3be61a03
wng1 <wng1@users.noreply.github.com>
2016-04-01 00:00:03 +0100

    Create five.sql

---
+++ variables/five.sql
@@ -0,0 +1,22 @@
+//TT-SQL   User Defined Function
+
+CREATE FUNCTION dbo.calculateAge
+(
+@DOB AS DATETIME
+@CURRENTDATE AS DATETIME
+)
+RETURN INT
+AS
+BEGIN
+  RETURN  DATEDIFF (year, @DOB, @CURRENTDATE) 
+  - CASE WHEN 100 * MONTH(@CURRENTDATE) + DAY(@CURRENTDATE)   
+  < 100 * MONTH(@DOB) + DAY(@DOB)
+  THEN 1
+  ELSE 0
+  END
+  END
+  GO
+  
+  
+  SELECT EmpID, FirstName, LastName, dbo.calculateAge(DOB, CURRENT_TIMESTAMP) AS Age
+  FROM HR.Employees
