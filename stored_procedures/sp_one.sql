139924bc7ec0cb2b8e06b07a20ad0ad8e4638394
wng1 <wng1@users.noreply.github.com>
2016-04-02 20:18:26 +0100

    Update sp_one.sql

--- stored_procedures/sp_one.sql
+++ stored_procedures/sp_one.sql
@@ -1,1 +1,17 @@
-CREATE
+CREATE PROCEDURE [dbo].[getComponentPriceOfMaterials]
+
+@PID int,
+@CheckDate DATETIME
+
+AS 
+BEGIN
+  SET NOCOUNT ON;
+  
+  WITH [CPOM_cte](ProdID, CID, CDescription, CCost, PerCQty, ListPrice)
+  AS
+  (
+  SELECT p.ProdID, p.CID, p.CDescription, pr.CCost, p.PerCQty, pr.ListPrice)
+  FROM Production.ComponentPriceOfMaterial p
+  INNER JOIN Production.Product pr
+  ON p.ProdID = pr.PID
+  
