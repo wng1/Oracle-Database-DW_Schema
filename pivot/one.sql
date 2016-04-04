281d38d2010c7d85946b26096f57d092f1cacdb8
wng1 <wng1@users.noreply.github.com>
2016-04-04 14:06:44 +0100

    Update one.sql

--- pivot/one.sql
+++ pivot/one.sql
@@ -1,11 +1,29 @@
 DECLARE @ColumnNames NVARCHAR(MAX) = ''
+DECLARE @SQL NVARCHAR(MAX) = ''
 
 SELECT @ColumnNames += QUOTENAME(Year) + ','
-FROM Orders
+FROM tblOrders
 
 SET @ColumnNames = LEFT(@ColumnNames,LEN(@ColumnNames) -1)
 
-PRINT @ColumnNames
+SET @SQL='
+SELECT * FROM
+( SELECT Year,
+         Country AS [ ],
+
+FROM tblOrders AS o 
+INNER JOIN tblCountry AS c ON c.countryID = o.orderCountryID)  AS Base
+
+PIVOT( COUNT(orderCountryID) 
+FOR YEAR
+IN (' + @ColumnNames + ')) AS PivotTable'
+
+EXECUTE @SQL
+
+
+
 
 //Result
-| 2010 | 2011 | 2012 | 2013 | 2014 | 2015 | 2016 |
+Country | 2010 | 2011 | 2012 | 2013 | 2014 | 2015 | 2016 |
+UK         1       0      0      1       1     1      1
+USA        2       1      3      5      4      1      2
