c421e9a041dac4fa8b8004872fd3555794264383
wng1 <wng1@users.noreply.github.com>
2016-04-09 16:17:05 +0100

    Update aggregateAmount.sql

--- pivot/aggregateAmount.sql
+++ pivot/aggregateAmount.sql
@@ -5,9 +5,33 @@ ProductID   | ProductName  | Total
 A02893        Dishes         292
 D02933        Foam           505
 F23923        PCB            1902
-
+G29328        Dishes         302
 
 SELECT ProductID, ProductName
 SUM(Amount) AS Total    //aggregates all the amount for each product
 FROM Sales.SalesInformation
 GROUP BY ProductID, ProductName  // sort order 
+
+
+//Multiple Product Names but different types can be reworded or better presented using cross tabs
+
+SELECT ProductID, Dishes, Foam, PCB  
+FROM Sales.SalesInformation
+PIVOT
+(
+
+SUM (Amount) 
+FOR ProductName
+IN Dishes, Foam PCB
+)
+
+AS PivotTable
+
+// output
+
+ProductID | Dishes  | Foam | PCB  //error occurence due to PK.   Change ProductID
+
+
+
+
+       
