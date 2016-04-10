7eabc29c3461ada03c34331d13dab68612736a6d
wng1 <wng1@users.noreply.github.com>
2016-04-11 00:01:55 +0100

    Update productsNotSold.sql

--- pivot/productsNotSold.sql
+++ pivot/productsNotSold.sql
@@ -4,3 +4,8 @@ LEFT JOIN Product.tblProductSales
 ON tblProduct.PID = tblProductSales.PID
 WHERE tblProductSales.PID IS NULL
 
+-- Highlights the products not been sold
+
+SELECT ProductName
+(SELECT SUM(NoOfProductsSold) FROM Product.tblProductSales WHERE PID = tblProduct.PID) AS QuantitySold
+FROM Product.tblProduct
